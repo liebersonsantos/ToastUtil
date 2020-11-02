0bda8e9ce7bd84b1b9cbc2d423e373f9037707ed
Lieberson  Xavier dos Santos <lieberson@lieberson.local>
2020-11-02 14:58:06 -0300

    fix

--- toast_library_example/src/main/java/br/com/liebersonsantos/toast_library_example/ToasterMessage.kt
+++ toast_library_example/src/main/java/br/com/liebersonsantos/toast_library_example/ToasterMessage.kt
@@ -5,6 +5,6 @@ import android.widget.Toast
 
 object ToasterMessage {
     fun showMessage(context: Context, message: String) {
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     }
 }
